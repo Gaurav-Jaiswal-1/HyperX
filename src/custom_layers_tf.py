d62ccd79955bedf2806c4ede866451b17ffada87
Gaurav Jaiswal <jaiswalgaurav863@gmail.com>
2025-05-27 11:11:39 +0530

    Remove all folders and files to reset repository

--- src/custom_layers_tf.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from src.hyperx_tf import HyperXActivation
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(64, input_shape=(10,)),
-    HyperXActivation(k=1.0),
-    tf.keras.layers.Dense(1)
-])
-model.compile(optimizer='adam', loss='mse')
